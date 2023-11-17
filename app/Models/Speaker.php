2f2825448a4e2e55ba0c95c3e9d32fd30f8a1b9e
Melville Spence <mel@cava.localdomain>
2023-11-17 21:52:19 +0000

    models created

---
+++ app/Models/Speaker.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Speaker extends Model
+{
+    use HasFactory;
+
+    protected $casts = [
+        'id' => 'integer',
+    ];
+
+    public function conferences(): BelongsToMany
+    {
+        return $this->belongsToMany(Conference::class);
+    }
+
+    public function talks(): HasMany
+    {
+        return $this->hasMany(Talk::class);
+    }
+}
